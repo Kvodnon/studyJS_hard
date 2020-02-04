3642916aeafe3698e3a5a96803da0c594b2a00c6
Kirill Kiselev <skulpin@mail.ru>
2020-02-04 21:51:39 +0700

    сложно

--- script.js
+++ script.js
@@ -1,50 +1,68 @@
 'use strict';
 
-let week = [
-    'Понедельник',
-    'Вторник',
-    'Среда',
-    'Четверг',
-    'Пятница',
-    'Суббота',
-    'Воскресенье'
-];
-
 let date = new Date();
 
-let div = document.createElement('div');
-
-let today = date.getDay() - 1;
-
-let getDay = function(counter, type) {
-    let day = document.createElement(type);
-    day.innerText = week[counter];
-
-    return day;
-};
-
-for (let counter = 0; counter < week.length; counter++) {
-    let par = document.createElement('p');
-
-    let day = week[counter];
-
-    if (counter === 5 || counter === 6) {
-        day = getDay(counter, 'em');
-    } 
-    
-    if (counter === today) {
-        day = getDay(counter, 'strong');
-    }
-
-    if (counter === today && counter === 5 || counter === 6) {
-        let em = document.createElement('em');
-        em.append(day);
-        day = em;
-    }
-
-    par.append(day);
-
-    div.append(par);
+function declOfNum(number, titles) {
+    let cases = [2, 0, 1, 1, 1, 2];
+    return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
 }
 
-document.body.append(div);
+let addZero = function(number) {
+    number += '';
+
+    if (number.length === 1) {
+        number = '0' + number;
+    }
+
+    return number;
+}
+
+// use:
+// declOfNum(count, ['найдена', 'найдено', 'найдены']);
+
+let options = {
+    era: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+    timezone: 'UTC',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+};
+
+let month = date.toLocaleString("ru", {
+        weekday: 'long'
+    }) + ',',
+    year = date.toLocaleString("ru", {
+        month: 'long',
+        day: 'numeric',
+    }) + ' года,',
+    hours = date.toLocaleString("ru", {
+        hour: 'numeric'
+    }),
+    minutes = date.toLocaleString("ru", {
+        minute: 'numeric'
+    }),
+    seconds = date.toLocaleString("ru", {
+        second: 'numeric'
+    });
+
+month = month.charAt(0).toUpperCase() + month.substring(1);
+hours += ' ' + declOfNum(hours, ['час', 'часа', 'часов']);
+minutes += ' ' + declOfNum(minutes, ['минута', 'минуты', 'минут']);
+seconds += ' ' + declOfNum(seconds, ['секунда', 'секунды', 'секунд']);
+
+// 2
+console.log('Сегодня', month, year, hours, minutes, seconds);
+
+let partOne = [addZero(date.getDate()), addZero(date.getMonth()), date.getFullYear()],
+    partTwo = [addZero(date.getHours()), addZero(date.getMinutes()), addZero(date.getSeconds())];
+
+// 3
+console.log(partOne.join('.'), '-',  partTwo.join(':'));
+
+setInterval(function() {
+    console.log(new Date());
+}, 1000);
